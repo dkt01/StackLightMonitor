b1dcc99e848c771f282e89bd12a70bcfe5529345
dkt01 <d.k.turner@att.net>
2017-08-11 14:25:46 -0500

    Added doxygen file headers with license info

--- Software/StackLightMonitor/StackLight.h
+++ Software/StackLightMonitor/StackLight.h
@@ -1,3 +1,24 @@
+////////////////////////////////////////////////////////////////////////////////
+/// @file StackLight.h
+///
+/// @brief Manages stack light output
+///
+/// @author David Turner (dkt01)
+///
+/// @copyright This file is part of StackLightMonitor
+///            (https://github.com/dkt01/StackLightMonitor).
+/// @copyright StackLightMonitor is free software: you can redistribute it and/or modify
+///            it under the terms of the GNU General Public License as published by
+///            the Free Software Foundation, either version 3 of the License, or
+///            (at your option) any later version.
+/// @copyright StackLightMonitor is distributed in the hope that it will be useful,
+///            but WITHOUT ANY WARRANTY; without even the implied warranty of
+///            MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+///            GNU General Public License for more details.
+/// @copyright You should have received a copy of the GNU General Public License
+///            along with StackLightMonitor.  If not, see <http://www.gnu.org/licenses/>.
+////////////////////////////////////////////////////////////////////////////////
+
 #include <Arduino.h>
 
 // Divisor to prevent overflows in period calculations
--- Software/StackLightMonitor/faviconConverter.sh
+++ Software/StackLightMonitor/faviconConverter.sh
@@ -1,5 +1,26 @@
 #!/usr/bin/env bash
 
+################################################################################
+### @file faviconConverter.sh
+###
+### @brief Converts favicon svg to ico encoded as a compressed http response
+###
+### @author David Turner (dkt01)
+###
+### @copyright This file is part of StackLightMonitor
+###            (https://github.com/dkt01/StackLightMonitor).
+### @copyright StackLightMonitor is free software: you can redistribute it and/or modify
+###            it under the terms of the GNU General Public License as published by
+###            the Free Software Foundation, either version 3 of the License, or
+###            (at your option) any later version.
+### @copyright StackLightMonitor is distributed in the hope that it will be useful,
+###            but WITHOUT ANY WARRANTY; without even the implied warranty of
+###            MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+###            GNU General Public License for more details.
+### @copyright You should have received a copy of the GNU General Public License
+###            along with StackLightMonitor.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
 FAVICON_SVG="favicon.svg"
 FAVICON_ICO="favicon.ico"
 FAVICON_HEADER="favicon_ico.h"
